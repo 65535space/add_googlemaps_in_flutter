56d14615bdbd8ed9e0d7f50a8f28bdfba2ee5d0c
space <93774811+65535space@users.noreply.github.com>
2024-07-05 11:48:01 +0900

    GoogleMAPAPIを設定し、環境変数化

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # add_googlemaps_into_flutter
 
-A new Flutter project.
-# add_googlemaps_into_flutter
+GooglemapsをFlutterに追加する方法を下記に残す
+1. fulutterプロジェクトのホームディレクトリに移動する
+
+2. 次のコマンドを実行する
+flutter pub add google_maps_flutter
+
+3. アンドロイドの最小バージョンを20以上にする
+android/app/build.gradle
+minsdkVersion 20
+
+4. AndroidアプリにAPIキーを追加する
+4.1　APIキーを作成(フィンガープリントについても下記のURLに記載)
+https://developers.google.com/maps/documentation/android-sdk/get-api-key?hl=ja
+
+5. android/app/src/main/AndroidManifest.xmlのmeta-dataに次の2つを指定する
+<meta-data android:name="com.google.android.geo.API_KEY"
+        android:value="YOUR-KEY-HERE"/> //←自分のAPIキーを設定する
+↑
+この位置をちゃんと確認すること!
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+  // LatLng はlatitude(緯度)とlongitude(経度)を表すクラス
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  // GoogleMapController オブジェクトを取得するためのコールバック関数
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        // GoogleMap ウィジェットを使って地図を表示
+        body: GoogleMap(
+          // プログラムからマップを操作するためのコントローラ
+          onMapCreated: _onMapCreated,
+          // 初期表示位置を指定
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
